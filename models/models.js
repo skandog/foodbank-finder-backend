fc425a00c8dec9946d4ebc4ad996417be8daf911
skandog <muumrar@gmail.com>
2022-08-02 16:25:34 +0100

    mongodb is evil. how do we get he data outgit add .! managed to make a little progress, but its still causing trouble when trying to acces the resultsdata in the route function

--- models/models.js
+++ models/models.js
@@ -1,5 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 import "dotenv/config";
+import fetch from "node-fetch";
 
 const uri = process.env.uri;
 
@@ -9,21 +10,38 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-client.connect(async (err, db) => {
-  if (err) throw err;
-  const collection = client.db("myFirstDatabase").collection("users");
-  // .find({ name: "skandy" })
-  // .toArray(function (err, result) {
-  //   if (err) throw err;
-  //   console.log(result);
-  //   db.close();
-  // });
+export async function getData(callback) {
+  client.connect(async (err, db) => {
+    if (err) throw err;
+    const collection = client.db("myFirstDatabase").collection("users");
+    // .find({ name: "skandy" })
+    // .toArray(function (err, result) {
+    //   if (err) throw err;
+    //   console.log(result);
+    //   db.close();
+    // });
 
-  const results = await collection
-    .find({ name: "camilla" })
-    
-    .toArray();
+    const results = await collection.find({ name: "camilla" }).toArray();
 
-  console.log("results :>> ", results); // perform actions on the collection object
-  client.close();
+    // console.log("results (model) :>> ", results); // perform actions on the collection object
+    // client.close();
+    return callback(results);
+  });
+}
+
+export let resultData = getData(function (results) {
+  console.log("results (new) :>> ", results);
 });
+
+// console.log(resultData)
+
+export const getDataApi = async (url) => {
+  const response = await fetch(url);
+
+  const data = await response.json();
+
+  //dev feedback only remove
+  console.log(`data in func >>>`, data);
+
+  return data;
+};
--- routes/items.js
+++ routes/items.js
@@ -1,10 +1,22 @@
 import express from "express";
 const router = express.Router();
+import { getData, getDataApi } from "../models/models.js";
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.json({ message: "I wish we had some information to give you ☹️" });
-});
+router.get("/", async function (req, res, next) {
+  // const result = await getDataApi(
+  //   "https://www.givefood.org.uk/api/2/foodbank/vauxhall"
+  // );
 
+  let resultData = await getData(function (results) {
+    console.log("results (moved) :>> ", results);
+    return results;
+  });
+
+  console.log("resultsData :>> ", resultData);
+  // let result = await resultData;
+  // // res.json({ message: "I wish we had some information to give you ☹️" });
+  // console.log("result (route):>> ", result);
+});
 
 export default router;
