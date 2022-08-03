9c9d8050a034b2976f85e8563f5452a03006ef6b
ClaudiaGC1339 <claudiagonzalez1339@gmail.com>
2022-08-03 14:14:56 +0100

    added a get route to get foodbanks based off of an address

--- routes/foodbanks.js
+++ routes/foodbanks.js
@@ -9,4 +9,13 @@ router.get("/", async (req, res) => {
   res.json(result);
 });
 
+router.get("/search/:address", async (req, res) => {
+  let address = req.params.address;
+  let url = `https://www.givefood.org.uk/api/2/foodbanks/search/?address=${address}`;
+
+  const result = await getDataApi(url);
+  res.json(result);
+
+});
+
 export default router;
