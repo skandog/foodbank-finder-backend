f867145fbf166eca7a6163ce9109fec9002b3db0
skandog <muumrar@gmail.com>
2022-08-05 10:44:58 +0100

    added trycatch to handle the problem with external search on api not returning some terms. works like a dream on first test

--- routes/foodbanks.js
+++ routes/foodbanks.js
@@ -1,20 +1,34 @@
 import express from "express";
 const router = express.Router();
 import { getDataApi } from "../models/models.js";
+let url = `https://www.givefood.org.uk/api/2/`;
 
 router.get("/", async (req, res) => {
-  let url = "https://www.givefood.org.uk/api/2/foodbanks";
+  let route = url + "foodbanks";
 
-  const result = await getDataApi(url);
+  const result = await getDataApi(route);
   res.json(result);
 });
 
 router.get("/search/:address", async (req, res) => {
   let address = req.params.address;
-  let url = `https://www.givefood.org.uk/api/2/foodbanks/search/?address=${address}`;
+  try {
+    let route = url + `foodbanks/search/?address=${address}`;
 
-  const result = await getDataApi(url);
-  res.json(result);
+    const result = await getDataApi(route);
+    res.json(result);
+    console.log("1st :>> ");
+  } catch {
+    try {
+      let route = url + `foodbank/${address}/`;
+
+      const result = await getDataApi(route);
+      res.json(result);
+      console.log("2nd :>> ");
+    } catch (error) {
+      res.json(error.message);
+    }
+  }
 });
 
 export default router;
